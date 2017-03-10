40f8e3bf2b55a3a2b59033d278537d7243f484cc
EmilyVacher <emily.vacher@theodi.org>
2017-03-10 15:10:12 +0000

    Corrected spellings and added 'About'

--- js/adapt-odi-licence.js
+++ js/adapt-odi-licence.js
@@ -28,22 +28,22 @@ define([
     getLicence: function(licence) {
 	if (licence == "CC-BY 4.0" ) {
 		url = "https://licensebuttons.net/l/by/4.0/88x31.png";
-		detail = "<span xmlns:dct='http://purl.org/dc/terms/' property='dct:title'>Learning open data</span> by <span xmlns:cc='http://creativecommons.org/ns# property='cc:attributionName'>Open Data Institute</span> is licensed under a Creative Commons Attribution 4.0 International License";
+		detail = "<span xmlns:dct='http://purl.org/dc/terms/' property='dct:title'>Learning open data</span> by <span xmlns:cc='http://creativecommons.org/ns# property='cc:attributionName'>Open Data Institute</span> is licensed under a Creative Commons Attribution 4.0 International Licence";
 	}
 	if (licence == "CC-BY-SA 4.0" ) {
 		url = "https://licensebuttons.net/l/by-sa/4.0/88x31.png";
-		detail = "<span xmlns:dct='http://purl.org/dc/terms/' property='dct:title'>Learning open data</span> by <span xmlns:cc='http://creativecommons.org/ns# property='cc:attributionName'>Open Data Institute</span> is licensed under a Creative Commons Attribution-ShareAlike 4.0 International License";
+		detail = "<span xmlns:dct='http://purl.org/dc/terms/' property='dct:title'>Learning open data</span> by <span xmlns:cc='http://creativecommons.org/ns# property='cc:attributionName'>Open Data Institute</span> is licensed under a Creative Commons Attribution-ShareAlike 4.0 International Licence";
 	}
 	string = "<div class='licencePageGraphicElement'><img class='licencePageGraphic' src='"+url+"'/></div>";
 	string += "<div class='licencePageText'>";
 	string += detail;
 	string += "<h2>Content usage</h2>";
-	string += "<p>The content, including text and structure is available for you to <em>use</em>, <em>reuse</em> and <em>share</em> (including for commercial purposes) subject to the conditions of the license above.</p>";
+	string += "<p>The content, including text and structure is available for you to <em>use</em>, <em>reuse</em> and <em>share</em> (including for commercial purposes) subject to the conditions of the licence above.</p>";
 	string += "<h2>Exclusions</h2>";
-	string += "<p>This license does not cover: <ul><li>Logos and trade marks belonging to the Open Data Institute</li><li>Third party content (such as images)</li></ul></p>";
-	string += "<h2>Non-endiresement</h2>";
+	string += "<p>This licence does not cover: <ul><li>Logos and trade marks belonging to the Open Data Institute</li><li>Third party content (such as images)</li><li>Any other content as outlined under 'About'</ul></p>";
+	string += "<h2>Non-endoresement</h2>";
 	string += "<p>This licence does not grant you any right to use the Information in a way that suggests any official status or that the Information Provider and/or Licensor endorse you or your use of the Information.</p>";
-	string += "<h2>No warrenty</h2>";
+	string += "<h2>No warranty</h2>";
 	string += "<p>The Information is licensed 'as is'. The Open Data Institute is not liable for any errors or omissions in the Information and shall not be liable for any loss, injury or damage of any kind caused by its use. The Information Provider does not guarantee the continued supply of the Information.</p>";
 	string += "</div>";
 	
