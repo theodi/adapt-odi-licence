b500e5eb2791b60bbfb3c85019e42dc13d67e3d4
Dave Tarrant <davetaz@theodi.org>
2017-08-15 19:20:27 +0100

    Bit updates to allow translation

--- js/adapt-odi-licence.js
+++ js/adapt-odi-licence.js
@@ -7,6 +7,7 @@ define([
     initialize: function() {
     	this.listenTo(Adapt, 'licencePage:showLicencePage', this.showLicencePage);
     	this.listenTo(Adapt, 'router:course', this.updateCourse);
+    	this.listenTo(Adapt, 'pageView:ready', this.addLink);
     },
 
     updateCourse: function(target) {
@@ -17,48 +18,39 @@ define([
     	return this.currentCourse;
     },
 
-    getCertificate: function() {
-	string = "<div class='licencePageGraphicElement'><img class='licencePageGraphic' src='adapt/css/assets/badge.png'/></div>";
-	string += "<div class='licencePageText'>";
-	string += "<ul class='open-data-certificate-details'> <li> <span>Open: ODI eLearning Data</span> </li> <li> <span>Pilot Open Data Certificate (final)</span> </li> <li> <span>Active - self certified</span> </li> </ul></div>";
-	string += "<hr class='licencePageRule'/>";
-	return string;
-    },
-    
-    getLicence: function(licence) {
-	if (licence == "CC-BY 4.0" ) {
-		url = "https://licensebuttons.net/l/by/4.0/88x31.png";
-		detail = "<p><span xmlns:dct='http://purl.org/dc/terms/' property='dct:title'>"+ Adapt.course.get('displayTitle') +" eLearning</span> by <span xmlns:cc='http://creativecommons.org/ns# property='cc:attributionName'>Open Data Institute</span> is licensed under a Creative Commons Attribution 4.0 International Licence</p>";
-	}
-	if (licence == "CC-BY-SA 4.0" ) {
-		url = "https://licensebuttons.net/l/by-sa/4.0/88x31.png";
-		detail = "<p><span xmlns:dct='http://purl.org/dc/terms/' property='dct:title'>"+Adapt.course.get('displayTitle')+" eLearning</span> by <span xmlns:cc='http://creativecommons.org/ns# property='cc:attributionName'>Open Data Institute</span> is licensed under a Creative Commons Attribution-ShareAlike 4.0 International Licence</p>";
-	}
-	string = "<div class='licencePageGraphicElement'><img class='licencePageGraphic' src='"+url+"'/></div>";
-	string += "<div class='licencePageText'>";
-	string += detail;
-	string += "<h2>Content usage</h2>";
-	string += "<p>The content, including text and structure is available for you to <em>use</em>, <em>reuse</em> and <em>share</em> (including for commercial purposes) subject to the conditions of the licence above.</p>";
-	string += "<h2>Exclusions</h2>";
-	string += "<p>This licence does not cover: <ul><li>Logos and trade marks belonging to the Open Data Institute</li><li>Third party content (such as images)</li><li>Any other content as outlined under 'About'</ul></p>";
-	string += "<h2>Non-endorsement</h2>";
-	string += "<p>This licence does not grant you any right to use the Information in a way that suggests any official status or that the Information Provider and/or Licensor endorse you or your use of the Information.</p>";
-	string += "<h2>No warranty</h2>";
-	string += "<p>The Information is licensed 'as is'. The Open Data Institute is not liable for any errors or omissions in the Information and shall not be liable for any loss, injury or damage of any kind caused by its use. The Information Provider does not guarantee the continued supply of the Information.</p>";
-	string += "</div>";
-	
-	return string;	
+    addLink: function() {
+        title = "Licence";
+        try {
+            title = Adapt.course.get('_globals')._extensions._licencePage.licenceLinkText;
+        } catch(err) {}
+        if( $('.about-links').prop('innerHTML').trim().length > 0) {
+            $('.about-links').append(' | ');
+        } 
+        $('.about-links').append('<a class="about" onClick=\'callLicencePageTrigger();\'>'+title+'</a>');
     },
     
     showLicencePage: function() {
-	string = "";
-  	licence = Adapt.config.get('_licencePage').licence;
-	console.log(this.getCourse());
-	//string = this.getCertificate();
-	string += this.getLicence(licence);
+		items = Adapt.course.get('_licencePage')._items;
+        title = "Licence";
+        try {
+            title = Adapt.course.get('_globals')._extensions._licencePage.titleText;
+        } catch(err) {}
+    	string = "";
+    	count = 1;
+ 		_.each(items, function(item) {
+			graphic = item._graphic;
+			if (graphic.src) {
+				string += "<div class='licencePageGraphicElement'><img class='licencePageGraphic' src='" + graphic.src + "'/></div>";
+			}
+	 		string += "<div class='licencePageText'>";
+			string += "<h2>" + item.title + "</h2>";
+ 			string += "<p>" + item.description + "</p>";
+			string += "</div>";
+ 			string += "<hr class='licencePageRule'/>";
+    	});
 	
-	var alertObject = {
-            title: "Licence",
+		var alertObject = {
+            title: title,
             body: string
         };
 
@@ -77,3 +69,8 @@ define([
 
   return (licencePage);
 });
+
+function callLicencePageTrigger() {
+    var Adapt = require('coreJS/adapt');
+    Adapt.trigger('licencePage:showLicencePage');
+}
