2e2330fd19e0d743556e2e2d6daae5a663e2e065
David Tarrant <davetaz@theodi.org>
2021-01-13 16:34:19 +0000

    Updating to work with Rollup as deployed in Framework 5.8

--- js/adapt-odi-licence.js
+++ js/adapt-odi-licence.js
@@ -29,33 +29,34 @@ define([
     },
 
     addLink: function() {
-        title = "Licence";
+        var title = "Licence";
         try {
             title = Adapt.course.get('_globals')._extensions._ODILicence.linkText;
         
             if( $('.about-links').prop('innerHTML').trim().length > 0) {
             	$('.about-links').append(' | ');
             } 
-            $('.about-links').append('<a class="about" onClick=\'callLicencePageTrigger();\'>'+title+'</a>');
+            $('.about-links').append('<a class="about" onClick="require(\'core/js/adapt\').trigger(\'licencePage:showLicencePage\'); return false;">'+title+'</a>');
 	} catch(err) {}
     },
     
     showLicencePage: function() {
-        items = [];
+        var items = [];
         try {
             items = this.contentObject.get('_ODILicence')._items;
         } catch(err) {}
         if (items.length < 1) {
             items = Adapt.course.get('_ODILicence')._items;
         }
-        title = "Licence";
+        
+        var title = "Licence";
         try {
             title = Adapt.course.get('_globals')._extensions._ODILicence.titleText;
         } catch(err) {}
-    	string = "";
-    	count = 1;
+    	var string = "";
+    	var count = 1;
  		_.each(items, function(item) {
-			graphic = item._graphic;
+			var graphic = item._graphic;
 			if (graphic.src) {
 				string += "<div class='licencePageGraphicElement'><img class='licencePageGraphic' src='" + graphic.src + "'/></div>";
 			}
@@ -86,8 +87,3 @@ define([
 
   return (licencePage);
 });
-
-function callLicencePageTrigger() {
-    var Adapt = require('core/js/adapt');
-    Adapt.trigger('licencePage:showLicencePage');
-}
